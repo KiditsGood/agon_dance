1804fe0c7b6026c5cc69c5e0e1e841b8c834316d
KiditsGood <71182254+KiditsGood@users.noreply.github.com>
2022-08-13 13:50:59 +0300

    agon v.2

--- js/directions.js
+++ js/directions.js
@@ -1,41 +1,94 @@
 $('.directions__european').mouseenter(function () {
+    if ($(window).width() < 1024) {
+        $('.directions__european-text').css({
+            display: 'none'
+        })
+
+        $('.european__list').css({
+            display: 'flex'
+        })
+    }
+    else {
+        $('.european__list').css({
+            display: 'block'
+        })
+    }
+
     $('.directions__european-text').addClass('directions__move')
-    $('.european__list').css({
-        display: 'block'
-    })
 
     $(this).mouseleave(function () {
         $('.directions__european-text').removeClass('directions__move')
         $('.european__list').css({
             display: 'none'
         })
+        if ($(window).width() < 1024) {
+            $('.directions__european-text').css({
+                display: 'block'
+            })
+        }
     })
 })
 
 $('.directions__latin').mouseenter(function () {
+    if ($(window).width() < 1024) {
+        $('.directions__latin-text').css({
+            display: 'none'
+        })
+
+        $('.latin__list').css({
+            display: 'flex'
+        })
+    }
+    else {
+        $('.latin__list').css({
+            display: 'block'
+        })
+    }
+
     $('.directions__latin-text').addClass('directions__move')
-    $('.latin__list').css({
-        display: 'block'
-    })
 
     $(this).mouseleave(function () {
         $('.directions__latin-text').removeClass('directions__move')
         $('.latin__list').css({
             display: 'none'
         })
+
+        if ($(window).width() < 1024) {
+            $('.directions__latin-text').css({
+                display: 'block'
+            })
+        }
     })
 })
 
 $('.directions__bot').mouseenter(function () {
+    if ($(window).width() < 1024) {
+        $('.directions__child').css({
+            display: 'none'
+        })
+
+        $('.child__list').css({
+            display: 'flex'
+        })
+    }
+    else {
+        $('.child__list').css({
+            display: 'block'
+        })
+    }
+
     $('.directions__child').addClass('directions__left')
-    $('.child__list').css({
-        display: 'block'
-    })
 
     $(this).mouseleave(function () {
         $('.directions__child').removeClass('directions__left')
         $('.child__list').css({
             display: 'none'
         })
+
+        if ($(window).width() < 1024) {
+            $('.directions__child').css({
+                display: 'block'
+            })
+        }
     })
 })
--- js/toggles.js
+++ js/toggles.js
@@ -14,3 +14,39 @@ $('.list__item-tops').click(function () {
 $('.nav__history').click(function () {
     $('.history__open').slideToggle('slow')
 })
+
+$('.burger').click(function () {
+    $(this).toggleClass('open')
+
+    $('.mobile').slideToggle('slow')
+
+    if ($('body').attr('style')) {
+        $('body').removeAttr('style')
+
+        $('#overlay').remove()
+    }
+    else {
+        $('body').css({
+            overflow: 'hidden'
+        })
+        $('body').append('<div id="overlay">')
+    }
+})
+
+$('.nav__item').click(function () {
+    if ($(window).width() < 1024) {
+        $('.mobile').slideToggle('slow')
+        $('.burger').toggleClass('open')
+    }
+    if ($('body').attr('style')) {
+        $('body').removeAttr('style')
+
+        $('#overlay').remove()
+    }
+    else {
+        $('body').css({
+            overflow: 'hidden'
+        })
+        $('body').append('<div id="overlay">')
+    }
+})
